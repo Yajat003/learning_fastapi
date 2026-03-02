b7849481a5e5201d501b79640c0859b75899e8dd
YDP <yajat1003@gmail.com>
2026-03-02 15:25:14 +0530

    Added 'put' and 'delete' endpoints

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Path, HTTPException, Query
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field, computed_field
-from typing import Annotated, Literal
+from typing import Annotated, Literal, Optional
 import json
 
 app = FastAPI()
@@ -9,24 +9,19 @@ app = FastAPI()
 # Pydantic Model
 class Patient(BaseModel):
 
-    id: Annotated[str, Field(..., description = "ID of the patient",
-                             examples = ["P001"])]
+    id: Annotated[str, Field(..., description = "ID of the patient", examples = ["P001"])]
     
     name: Annotated[str, Field(..., description = "Name of the patient")]
 
-    city: Annotated[str, Field(..., 
-                               description = "city where the patient is living")]
+    city: Annotated[str, Field(..., description = "city where the patient is living")]
     
-    age: Annotated[int, Field(..., gt = 0, lt = 120, 
-                              description = "Age of the patient")]
+    age: Annotated[int, Field(..., gt = 0, lt = 120, description = "Age of the patient")]
     
-    gender: Annotated[Literal['male', 'female', 'others'], 
-                      Field(..., description = "Gender of the patient")]
+    gender: Annotated[Literal['Male', 'Female', 'Others'], Field(..., description = "Gender of the patient")]
     
-    height: Annotated[float, Field(..., gt = 0, 
-                                   description = "Height of the patient in metres(M)")]
-    weight: Annotated[float, Field(..., gt = 0, 
-                                   description = "Weight of the patient in kilograms(KG)")]
+    height: Annotated[float, Field(..., gt = 0, description = "Height of the patient in metres(M)")]
+    
+    weight: Annotated[float, Field(..., gt = 0, description = "Weight of the patient in kilograms(KG)")]
 
     @computed_field
     @property
@@ -45,6 +40,20 @@ class Patient(BaseModel):
             return "Normal"
         else:
             return "Overweight"
+
+class PatientUpdate(BaseModel):
+    
+    name: Annotated[Optional[str], Field(default = None)]
+
+    city: Annotated[Optional[str], Field(default = None)]
+
+    age: Annotated[Optional[int], Field(default = None, gt = 0)]
+
+    gender: Annotated[Optional[Literal['Male', 'Female','Others']], Field(default = None)]
+
+    height: Annotated[Optional[float], Field(default = None, gt = 0)]
+
+    weight: Annotated[Optional[float], Field(default = None, gt = 0)]
 
 def load_data():
     with open('patients.json', 'r') as f:
@@ -124,8 +133,50 @@ def create_patient(patient: Patient):
     return JSONResponse(status_code = 201, 
                         content = "Patient created successfully")
 
-
+@app.put('/edit/{patient_id}')
+def update_patient(patient_id: str, patient_update: PatientUpdate):
     
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code = 404, detail = "Patient not found")
+    
+    existing_patient_info = data[patient_id]
+    
+    updated_patient_info = patient_update.model_dump(exclude_unset = True)
+
+    for key, value in updated_patient_info.items():
+        existing_patient_info[key] = value
+    
+    # existing patient info -> pydantic object -> updated bmi & weight ->
+    existing_patient_info['id'] = patient_id
+    patient_pydantic_obj = Patient(**existing_patient_info)
+
+    # -> pydantic object -> dict
+    existing_patient_info = patient_pydantic_obj.model_dump(exclude = 'id')
+
+    # add this dict to json data
+    data[patient_id] = existing_patient_info
+
+    # save data
+    save_data(data)
+
+    return JSONResponse(status_code = 200, content = {"message":"patient updated"})
+
+@app.delete('/delete/{patient_id}')
+def delete_patient(patient_id: str):
+
+    data = load_data()
+
+    if patient_id not in  data:
+        raise HTTPException(status_code = 404, detail = "Patient not found")
+    
+    del data[patient_id]
+
+    save_data(data)
+
+    return JSONResponse(status_code = 200, content = {"message":"patient deleted"})
+
 
     
 '''
